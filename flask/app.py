db452bfb16fa590bdee1684f12ef5572700363d4
Sen-Yang2005 <2961134Y@student.gla.ac.uk>
2024-03-18 15:51:55 +0000

    define each page route
    home_page
    about_page
    questionnaire_page

---
+++ flask/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('home_page')
+
+@app.route('/about')
+def about_page():
+    return render_template('about_page.html')
+
+@app.route('/questionnaire')
+def questionnaire_page():
+    return render_template('questionnaire_page.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
